6ee3d2627934bfcdbaad6e47ce0ababfbcc879b6
toni tegar sahidi <tonitegarsahidi@gmail.com>
2021-03-27 05:36:43 +0700

    Initial commit

---
+++ include/config.php
@@ -0,0 +1,12 @@
+<?php
+    $_CONFIG_MAINURL = "https://ninjaqr.com";
+
+    //seo thing
+    $_CONFIG_SITETITLE = "#1 Best Online QR Code Scanner and Generator";
+    $_CONFIG_SITEDESCRIPTION = "Ninja QR is Free Online QR Code Scanner and QR Code Generator. Click and use. No Install!";
+    $_CONFIG_SITEKEYWORD = "QR Code, QR Code Scanner, QR Code Generator, NinjaQR, QRCode";
+
+    $_CONFIG_LANGFILE = "lang/lang-en.php";
+    $_CONFIG_LANG = "es";
+    $_CONFIG_CHARSET = "utf-8";
+?>
---
+++ include/header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<?php
+
+include "config.php";
+?>
+
+<!---
+Copyright 2017 The AMP Start Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+<html ⚡="" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title><?=$_CONFIG_SITETITLE?></title>
+    <link
+      rel="canonical"
+      href="https://www.ampstart.com/templates/e-commerce/landing.amp"
+    />
+    <meta name="viewport" content="width=device-width" />
+    <meta name="amp-google-client-id-api" content="googleanalytics" />
+
+    <script async="" src="https://cdn.ampproject.org/v0.js"></script>
+
+    <style amp-boilerplate="">
+      body {
+        -webkit-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -moz-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        -ms-animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+        animation: -amp-start 8s steps(1, end) 0s 1 normal both;
+      }
+      @-webkit-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-moz-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-ms-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @-o-keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+      @keyframes -amp-start {
+        from {
+          visibility: hidden;
+        }
+        to {
+          visibility: visible;
+        }
+      }
+    </style>
+    <noscript> 
+      <style amp-boilerplate="">
+        body {
+          -webkit-animation: none;
+          -moz-animation: none;
+          -ms-animation: none;
+          animation: none;
+        }
+      </style>
+    </noscript>
+
+    <script
+      custom-element="amp-sidebar"
+      src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"
+      async=""
+    ></script>
+
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700"
+      rel="stylesheet"
+    />
+
+
+    <style amp-custom="">
+    <?php
+      include "css/page.css";
+    ?>
+
+    </style>
+
+  </head>
+  <body>
